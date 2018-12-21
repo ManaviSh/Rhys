43862a7ea43dd4b31aaacfb37a3789c9e9357fb4
Manavi Shukla <manavi139@gmail.com>
2018-12-21 20:42:39 +0530

    Add files via upload

---
+++ Face_detect.m
@@ -0,0 +1,14 @@
+%read the image
+A = imread('test.jpg');
+%Get FaceDetector Object
+FaceDetector = vision.CascadeObjectDetector();
+%Use Face_detector object on A get faces
+bbox = step(FaceDetector, A);
+%Annotate these faces on the top of the image
+B = insertObjectAnnotation(A, 'rectangle', bbox, 'face');
+imshow(B), title('Detected Faces');
+%display the no of facs in a string
+n = size(bbox, 1);
+str_n = num2str(n);
+str = strcat('Number of detected faces are', str_n);
+disp(str);
